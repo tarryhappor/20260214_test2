54b6398a0a588f9ebbf2d871a287a48bba9db281
tarryhappor <s1b.33310@gmail.com>
2026-02-15 19:41:50 +0900

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -2,21 +2,28 @@ function calculateResult(){
 
 let scores = {E:0,I:0,R:0,S:0,P:0,F:0,A:0,C:0};
 
-document.querySelectorAll("input[type=radio]:checked").forEach(input=>{
-  let q = parseInt(input.name.replace("q",""));
-  let val = input.value;
+const map = [
+["E","I"],["I","E"],["E","I"],["I","E"],
+["R","S"],["S","R"],["R","S"],["S","R"],
+["P","F"],["F","P"],["P","F"],["F","P"],
+["C","A"],["C","A"],["A","C"],["C","A"]
+];
 
-  const map = [
-  ["E","I"],["I","E"],["E","I"],["I","E"],
-  ["R","S"],["S","R"],["R","S"],["S","R"],
-  ["P","F"],["F","P"],["P","F"],["F","P"],
-  ["C","A"],["C","A"],["A","C"],["C","A"]
-  ];
+// 16問すべて読む
+for(let i=0;i<16;i++){
 
-  if(val==="A") scores[map[q][0]]++;
-  else scores[map[q][1]]++;
-});
+  let answer = localStorage.getItem("q"+i);
 
+  if(!answer) continue;
+
+  if(answer==="A"){
+    scores[map[i][0]]++;
+  }else{
+    scores[map[i][1]]++;
+  }
+}
+
+// 4軸判定
 let type =
  (scores.E>=scores.I?"E":"I")+
  (scores.R>=scores.S?"R":"S")+
@@ -24,5 +31,6 @@ let type =
  (scores.A>=scores.C?"A":"C");
 
 localStorage.setItem("resultType",type);
+
 window.location.href="result.html";
 }
